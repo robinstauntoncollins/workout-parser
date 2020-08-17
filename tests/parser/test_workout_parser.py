180d061ee638986616e20f8cf9487f45e82fb49b
Robin Staunton-Collins <robinstauntoncollins@gmail.com>
2020-08-17 16:26:27 +0200

    Extracted exercises

--- tests/parser/test_workout_parser.py
+++ tests/parser/test_workout_parser.py
@@ -8,14 +8,14 @@ import pytest
 
 class TestWorkoutParser():
         
-    test_data = [
+    full_workout_data = [
         ('./data/20200803.txt', './data/20200803.json'),
         # ('./data/20200814.txt', './data/20200814.json'),
         # ('./data/recommended_routine.txt', './data/recommended_routine.json'),
         # ('./data/starting_strength_example_workout.txt', './data/starting_strength_example_workout.json'),
     ]
 
-    @pytest.mark.parametrize("workout_path,expected_path", test_data)
+    @pytest.mark.parametrize("workout_path,expected_path", full_workout_data)
     def test_workout_parser(self, workout_path, expected_path):
         with open(workout_path) as f:
             workout_raw = f.read()
@@ -26,7 +26,7 @@ class TestWorkoutParser():
         result = WorkoutParser(workout_raw).parse()
         assert result == expected
 
-    test_data = [
+    date_data = [
         ("""3 August 2020\nSomeother Stuff and numbers: 21041""", "3 August 2020"),
         ("""6th August 2020\n\nWarmup:\nBand Shoulder routine: x10""", "6th August 2020"),
         ("3 August 2020\nSomeot", "3 August 2020"),
@@ -37,8 +37,37 @@ class TestWorkoutParser():
         ("2nd October 2020\nSomeot", "2nd October 2020"),
     ]
 
-    @pytest.mark.parametrize("date_text,expected", test_data)
+    @pytest.mark.parametrize("date_text,expected", date_data)
     def test_extract_date(self, date_text, expected):
         result = WorkoutParser(date_text)._extract_date()
+        assert result == expected
+
+    section_data = [
+        (
+            ['3 August 2020', 'Warm-up:', 'Assisted Arch Hangs: x5', 'Pair 1:', 'Pullups: x5 x5 x5'],
+            {
+                'Warm-up:': [
+                    'Assisted Arch Hangs: x5'
+                ],
+                'Pair 1:': [
+                    'Pullups: x5 x5 x5'
+                ]
+            }
+        )
+    ]
+
+    @pytest.mark.parametrize("lines,expected", section_data)
+    def test_extract_sections(self, lines, expected):
+        result = WorkoutParser(None)._extract_sections(lines)
+        assert result == expected
+
+    exercise_data = [
+        ('Assisted Arch Hangs: x5', ("Assisted Arch Hangs", "x5")),
+        ('Pullups: x5 x5 x5', ("Pullups", "x5 x5 x5")),
+    ]
+
+    @pytest.mark.parametrize("text,expected", exercise_data)
+    def test_extract_exercise(self, text, expected):
+        result = WorkoutParser(None)._extract_exercise(text)
         assert result == expected
     
--- workout/parser/workout_parser.py
+++ workout/parser/workout_parser.py
@@ -1,16 +1,81 @@
 import re
+from typing import List
 
 class WorkoutParser():
+
+    patterns = {
+        'date': r"[\d]{1,2}(?:[trns]\w*)? [ADFJMNOS]\w* [\d]{4}",
+        'lines': r".*\S.*",
+        'section_header': r"^.*:$",
+        'exercise': r"(.+):(.+)"
+    }
     
     def __init__(self, raw):
         self.raw = raw
 
     def parse(self):
         date = self._extract_date()
-        return date
+        lines = self._extract_lines()
+        print(f"Lines: {lines}")
+        sections = self._extract_sections(lines)
+        print(f"Sections: {sections}")
+        self.exercises = self._extract_exercise
+        exercises = [
+            {
+                "section": section,
+                "name": self._extract_exercise(exercise)[0]
+            }
+            for section, exercises in sections.items()
+            for exercise in exercises
+        ]
+        return {
+            'date': date,
+            'exercises': exercises
+        }
 
     def _extract_date(self):
-        pattern = r"[\d]{1,2}(?:[trns]\w*)? [ADFJMNOS]\w* [\d]{4}"
-        p = re.compile(pattern)
+        p = re.compile(self.patterns['date'])
         return p.search(self.raw).group()
+
+    def _extract_lines(self):
+        p = re.compile(self.patterns['lines'])
+        return re.findall(p, self.raw)
+
+    def _is_section_header(self, text: str):
+        p = re.compile(self.patterns['section_header'])
+        return bool(p.match(text))
+
+    def _extract_sections(self, lines: List[str]) -> dict:
+        sections = {}
+        section = None
+        for i, line in enumerate(lines):
+            if self._is_section_header(line):
+                section = line
+            else:
+                if not section:
+                    continue
+                else:
+                    if section not in sections:
+                        sections[section] = [line]
+                    else:
+                        sections[section].append(line)
+        self.sections = sections
+        return sections
+
+    def _extract_exercise(self, text: str):
+        print(f"Received: text: {text}")
+        if type(text) != str:
+            raise ValueError(f"Got {type(text)} expected 'str'")
+        p = re.compile(self.patterns['exercise'])
+        matches = p.search(text)
+        if not matches:
+            raise ValueError(f"Did not match string: {text}")
+        name = matches.group(1)
+        setrep_string = matches.group(2)
+        return (name.strip(), setrep_string.strip())
+
+
+        
+
+
     
