a14f4692633602273936bd9597c0947e8fdbb319
Robin Staunton-Collins <robinstauntoncollins@gmail.com>
2020-08-19 17:32:23 +0200

    Adds exercise string parsing

--- tests/parser/test_workout_parser.py
+++ tests/parser/test_workout_parser.py
@@ -42,17 +42,63 @@ class TestWorkoutParser():
         result = WorkoutParser(date_text)._extract_date()
         assert result == expected
 
+    def test_extract_lines(self):
+        # TODO
+        pass
+
+    is_section_header_test_data = [
+        ('Warmup:', True),
+        ('Warm-up:', True),
+        ('Pair 1:', True),
+        ('Assisted Arch Hangs: x5', False),
+        ('Pullups: x5 x5 x5', False),
+        ('BB Squat (50kg+bar): x5 x5 x5', False),
+        ('Rest: 2 mins', False),
+        ('Rest: 90s', False),
+        ('Rest 90s', False),
+    ]
+    
+    @pytest.mark.parametrize("text,expected", is_section_header_test_data)
+    def test_is_section_header(self, text, expected):
+        result = WorkoutParser(None)._is_section_header(text)
+        assert result == expected
+
     section_data = [
         (
-            ['3 August 2020', 'Warm-up:', 'Assisted Arch Hangs: x5', 'Pair 1:', 'Pullups: x5 x5 x5'],
-            {
-                'Warm-up:': [
-                    'Assisted Arch Hangs: x5'
-                ],
-                'Pair 1:': [
-                    'Pullups: x5 x5 x5'
-                ]
-            }
+            [
+                '3 August 2020',
+                'Warm-up:',
+                'Assisted Arch Hangs: x5',
+                'Pair 1:',
+                'Pullups: x5 x5 x5',
+                'Rest: 90s',
+                'Pair 2:',
+                'BB Squat (50kg+bar): x5 x5 x5',
+                'Rest 2 mins',
+            ],
+            [
+                {
+                    'name': 'Warm-up:',
+                    'exercises': [
+                        'Assisted Arch Hangs: x5',
+                    ],
+                    'rest': None
+                },
+                {
+                    'name': 'Pair 1:',
+                    'exercises': [
+                        'Pullups: x5 x5 x5',
+                    ],
+                    'rest': 90
+                },
+                {
+                    'name': 'Pair 2:',
+                    'exercises': [
+                        'BB Squat (50kg+bar): x5 x5 x5',
+                    ],
+                    'rest': 120
+                }
+            ]
         )
     ]
 
@@ -61,9 +107,30 @@ class TestWorkoutParser():
         result = WorkoutParser(None)._extract_sections(lines)
         assert result == expected
 
+    rest_string_test_data = [
+        ('Rest: 90s', 90),
+        ('Rest: 2 mins', 120),
+        ('Rest 90s', 90),
+        ('Rest 2mins', 120)
+    ]
+
+    @pytest.mark.parametrize("text,expected", rest_string_test_data)
+    def test_parse_rest_string(self, text, expected):
+        result = WorkoutParser(None)._parse_rest_string(text)
+        assert result == expected
+
+
     exercise_data = [
-        ('Assisted Arch Hangs: x5', ("Assisted Arch Hangs", "x5")),
-        ('Pullups: x5 x5 x5', ("Pullups", "x5 x5 x5")),
+        ('Assisted Arch Hangs: x5', {"name": "Assisted Arch Hangs", "reps": [5]}),
+        ('Assisted Arch Hangs: x10', {"name": "Assisted Arch Hangs", "reps": [10]}),
+        ('Pullups: x5 x5 x5', {"name": "Pullups", "reps": [5, 5, 5]}),
+        (
+            'Ring Dips: x3+2 negs x3+2 negs x2+3negs',
+            [
+                {"name": "Ring Dips", "reps": [3, 3, 2]},
+                {"name": "Ring Dips negatives", "reps": [2, 2, 3]}
+            ]
+        )
     ]
 
     @pytest.mark.parametrize("text,expected", exercise_data)
--- workout/parser/workout_parser.py
+++ workout/parser/workout_parser.py
@@ -7,7 +7,9 @@ class WorkoutParser():
         'date': r"[\d]{1,2}(?:[trns]\w*)? [ADFJMNOS]\w* [\d]{4}",
         'lines': r".*\S.*",
         'section_header': r"^.*:$",
-        'exercise': r"(.+):(.+)"
+        'exercise': r"(.+):(.+)",
+        'rest': r"(?:\w+(?::)?\s)([\d]{1,2}\s?)(\w{1,4})",
+        'exercise_reps': r"(?:x)(\d{1,2})(?:\+(\d\s?negs?))?"
     }
     
     def __init__(self, raw):
@@ -19,18 +21,11 @@ class WorkoutParser():
         print(f"Lines: {lines}")
         sections = self._extract_sections(lines)
         print(f"Sections: {sections}")
-        self.exercises = self._extract_exercise
-        exercises = [
-            {
-                "section": section,
-                "name": self._extract_exercise(exercise)[0]
-            }
-            for section, exercises in sections.items()
-            for exercise in exercises
-        ]
+        exercise_strings = [ex for section in sections for ex in section['exercises']]
+        self.exercises = [self._extract_exercise(ex) for ex in exercise_strings]
         return {
             'date': date,
-            'exercises': exercises
+            'exercises': self.exercises
         }
 
     def _extract_date(self):
@@ -46,36 +41,62 @@ class WorkoutParser():
         return bool(p.match(text))
 
     def _extract_sections(self, lines: List[str]) -> dict:
-        sections = {}
-        section = None
+        sections = []
+        section = {'name': None, 'exercises': [], 'rest': None}
         for i, line in enumerate(lines):
+            print(line)
             if self._is_section_header(line):
-                section = line
+                if section['name'] is not None:
+                    sections.append(section)
+                    section = {'name': None, 'exercises': [], 'rest': None}
+                section['name'] = line
+            elif 'rest' in line.lower():
+                section['rest'] = self._parse_rest_string(line)
+                sections.append(section)
+                section = {'name': None, 'exercises': [], 'rest': None}
             else:
-                if not section:
-                    continue
+                if section['name'] is not None:
+                    section['exercises'].append(line)
                 else:
-                    if section not in sections:
-                        sections[section] = [line]
-                    else:
-                        sections[section].append(line)
-        self.sections = sections
+                    continue
         return sections
+
+    def _parse_rest_string(self, string: str) -> int:
+        p = re.compile(self.patterns['rest'])
+        m = p.match(string)
+        val, unit = m.groups()
+        if unit == 'mins':
+            return int(val) * 60
+        return int(val)
+
 
     def _extract_exercise(self, text: str):
         print(f"Received: text: {text}")
         if type(text) != str:
             raise ValueError(f"Got {type(text)} expected 'str'")
-        p = re.compile(self.patterns['exercise'])
-        matches = p.search(text)
-        if not matches:
-            raise ValueError(f"Did not match string: {text}")
-        name = matches.group(1)
-        setrep_string = matches.group(2)
-        return (name.strip(), setrep_string.strip())
 
-
+        name, reps = text.split(':')
         
+        p = re.compile(self.patterns['exercise_reps'])
+        matches = p.findall(reps)
+        if not matches:
+            print(f"Not an exercise: {text}")
+            return
+        reps = [int(pair[0]) for pair in matches]
+        return {"name": name.strip(), "reps": reps}
 
 
-    
+if __name__ == '__main__':
+    lines = [
+                '3 August 2020',
+                'Warm-up:',
+                'Assisted Arch Hangs: x5',
+                'Pair 1:',
+                'Pullups: x5 x5 x5',
+                'Rest: 90s',
+                'Pair 2:',
+                'BB Squat (50kg+bar): x5 x5 x5',
+                'Rest 2 mins',
+    ]
+    result = WorkoutParser(None)._extract_sections(lines)
+    print(result)
